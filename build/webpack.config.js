483a392c85a9ce35d6f912cd3f34993003a9aa41
Polly <973367375@qq.com>
2017-12-04 14:47:48 +0800

    add new things

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -3,67 +3,85 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FreindlyErrors = require('friendly-errors-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const isProd = process.env.NODE_ENV === 'production'
+console.log(process.env.NODE_ENV)
 // const CleanWebpackPlugin = require('clean-webpack-plugin')
-
+const plugins = isProd ? [
+  new ExtractTextPlugin('css/[name].css'),
+  new HtmlWebpackPlugin({
+    template: 'index.html',
+    filename: 'index.html'
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+      drop_console: true
+    }
+  })
+] : [
+  new webpack.HotModuleReplacementPlugin(),
+  new FreindlyErrors(),
+  new HtmlWebpackPlugin({
+    template: 'index.html',
+    filename: 'index.html'
+  })
+]
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'app.js',
-		path: path.resolve(__dirname, '../js')
-	},
-	devtool: 'inline-source-map',
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							babelrc: false,
-							presets: ['babel-preset-env'],
-							plugins: ['babel-plugin-transform-runtime'],
-							// You must run npm install babel-plugin-transform-runtime --save-dev to include this in your project and babel-runtime itself as a dependency with npm install babel-runtime --save.
-						}
-					}
-				],
-				exclude:/node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader',
-				]
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: [
-					{
-						loader:'file-loader',
-						options: {
-							outputPath: path.resolve(__dirname,'../medias/images')
-						}
-					}
-				]
-			}
-		]
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new FreindlyErrors(),
-		new HtmlWebpackPlugin({
-			template: 'index.html',
-			filename: 'index.html'
-		}),
-	],
-	// in development
-	devServer: {
-		hot: true,
-		contentBase: path.resolve(__dirname, '../'),
-		host: '0.0.0.0',
-		disableHostCheck: true,
-		port: 3000,
-		quiet: true,
-	},
+  entry: './src/index.js',
+  output: {
+    filename: 'js/app.js',
+    path: path.resolve(__dirname, '../')
+  },
+  devtool: !isProd ? 'inline-source-map' : undefined,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              presets: ['babel-preset-env'],
+              plugins: ['babel-plugin-transform-runtime'],
+            // You must run npm install babel-plugin-transform-runtime --save-dev to include this in your project and babel-runtime itself as a dependency with npm install babel-runtime --save.
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: !isProd ? [
+          'style-loader',
+          'css-loader'
+        ] : ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: path.resolve(__dirname, '../medias/images')
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins,
+  // in development
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, '../'),
+    host: '0.0.0.0',
+    disableHostCheck: true,
+    port: 3000,
+    quiet: true
+  }
 }
